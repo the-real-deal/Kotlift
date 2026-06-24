2b408c43b1debccedba5e3f11b630158d6f7ae2d
Gioele Foschi <gioele.foschi.14@gmail.com>
2026-06-24 12:11:33 +0200

    Removed unused functions

--- app/src/main/java/com/therealdeal/kotlift/ui/composables/headers/ProfileHeaderSection.kt
+++ app/src/main/java/com/therealdeal/kotlift/ui/composables/headers/ProfileHeaderSection.kt
@@ -1,11 +1,5 @@
 package com.therealdeal.kotlift.ui.composables.headers
 
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,14 +18,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -69,30 +58,4 @@ fun ProfileHeaderSection(user: Profile?) {
     }
 }
 
-fun Modifier.shimmerEffect(): Modifier = composed {
-    val transition = rememberInfiniteTransition(label = "shimmer")
-    val translateAnim by transition.animateFloat(
-        initialValue = 0f,
-        targetValue = 1000f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = 1200, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        ),
-        label = "shimmer"
-    )
 
-    // Customize these colors based on your primary theme color
-    val shimmerColors = listOf(
-        Color.White.copy(alpha = 0.15f),
-        Color.White.copy(alpha = 0.4f),
-        Color.White.copy(alpha = 0.15f),
-    )
-
-    val brush = Brush.linearGradient(
-        colors = shimmerColors,
-        start = Offset.Zero,
-        end = Offset(x = translateAnim, y = translateAnim)
-    )
-
-    return@composed this.background(brush)
-}
