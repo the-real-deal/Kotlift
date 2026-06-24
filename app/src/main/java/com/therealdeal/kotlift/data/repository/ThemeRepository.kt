87138e750c0a4e2bee22875aefc65e237a7ce4dc
Gioele Foschi <gioele.foschi.14@gmail.com>
2026-06-24 18:20:33 +0200

    Removing residuals from system theme

--- app/src/main/java/com/therealdeal/kotlift/data/repository/ThemeRepository.kt
+++ app/src/main/java/com/therealdeal/kotlift/data/repository/ThemeRepository.kt
@@ -2,7 +2,6 @@ package com.therealdeal.kotlift.data.repository
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.therealdeal.kotlift.model.Theme
